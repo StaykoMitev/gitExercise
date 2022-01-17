06464d1c0f659b6689ed8758a29cafbe048c3bd8
zzx <staykod.mitev@gmail.com>
2022-01-17 22:47:49 +0200

    commit a

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -3,6 +3,6 @@ package com.test;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("zzx change");
+        System.out.println("asdasd change");
     }
 }
