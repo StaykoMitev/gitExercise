2057692563bc8ff7cde3ebbc53830544a14e1684
zzx <staykod.mitev@gmail.com>
2022-01-17 21:54:03 +0200

    second commit

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -3,6 +3,6 @@ package com.test;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Stayko's branch");
+        System.out.println("Main class");
     }
 }
