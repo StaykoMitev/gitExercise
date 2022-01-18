a0822b89479025095fecdbe43307a1b388af57bc
ivayloslavkov <ivayloslavkov@gmail.com>
2022-01-18 09:50:31 +0200

    Commit merge

--- src/com/test/ClassNAN.java
+++ src/com/test/ClassNAN.java
@@ -4,6 +4,7 @@ public class ClassNAN {
 
         public static void main (String[] args) {
             System.out.println ("Conflict change");
+            System.out.println ("Second change");
         }
     }
 
