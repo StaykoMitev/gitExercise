7ea9fbac901062ff790c618d1192e0faa5e1b982
ivayloslavkov <ivayloslavkov@gmail.com>
2022-01-18 09:19:29 +0200

    Commit 2

--- src/com/test/ClassIKS.java
+++ src/com/test/ClassIKS.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class ClassIKS {
     public static void main(String[] args) {
-        System.out.println("Initial change");
+        System.out.println("Second change");
     }
 }
