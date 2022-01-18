fe1ff93da601a7e9e44297816a04dbe6628a0649
SokolTsvetanov <stsvetanov@nextforhr.com>
2022-01-18 09:19:34 +0200

    Initial commit - first file added

--- src/com/test/ClassSST.java
+++ src/com/test/ClassSST.java
@@ -3,7 +3,7 @@ package com.test;
 public class ClassSST {
     public static void main(String[] args) {
         {
-            System.out.println("Initial change");
+            System.out.println("Second change");
         }
     }
 }
