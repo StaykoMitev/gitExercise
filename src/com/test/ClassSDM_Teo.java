8f14291b1de92371f69c72d41581e329b7004e49
tbandarmaliev <tbandarmaliev@nextforhr.com>
2022-01-18 09:19:07 +0200

    Test

--- src/com/test/ClassSDM_Teo.java
+++ src/com/test/ClassSDM_Teo.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class ClassSDM_Teo {
     public static void main(String[] args) {
-        System.out.println("Initial change");
+        System.out.println("Second change");
     }
 }
