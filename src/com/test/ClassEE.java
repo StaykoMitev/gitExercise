1864b991239271bd92c59441b1383f263a85ffd3
eevtimov <97660172+eevtimov@users.noreply.github.com>
2022-01-18 09:19:13 +0200

    TestCommit

--- src/com/test/ClassEE.java
+++ src/com/test/ClassEE.java
@@ -2,8 +2,7 @@ package com.test;
 
 public class ClassEE {
     public static void main(String[] args) {
-        System.out.println("Initial change");
+        System.out.println("Second Change");
     }
-
 
 }
