53009a846ab77d07ea68a623d1510da565c400a1
ivayloslavkov <ivayloslavkov@gmail.com>
2022-01-18 09:33:22 +0200

    Commit 3

--- src/com/test/ClassNAN.java
+++ src/com/test/ClassNAN.java
@@ -3,7 +3,7 @@ package com.test;
 public class ClassNAN {
 
         public static void main (String[] args) {
-            System.out.println ("Second change");
+            System.out.println ("Conflict change");
         }
     }
 
