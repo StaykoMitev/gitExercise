940537c86a7f972ac08a7a3b4eeed6c169de02a1
BiseraRuzhina <97660163+BiseraRuzhina@users.noreply.github.com>
2022-01-19 18:38:35 +0200

    Message changed to "Conlict change" in order to make a conflict

--- src/com/test/ClassMKG.java
+++ src/com/test/ClassMKG.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class ClassMKG {
     public static void main(String[] args) {
-        System.out.println("Conflict change");
+        System.out.println("Conflict change second");
     }
 }
