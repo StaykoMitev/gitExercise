e1e8968e3d4c1f074f134a27dfac26dce23dac89
dimabetova <dimabetova@abv.bg>
2022-01-19 18:40:48 +0200

    Commit Conflict change 02

--- src/com/test/ClassBGR.java
+++ src/com/test/ClassBGR.java
@@ -3,6 +3,6 @@ package com.test;
 public class ClassBGR {
     public static void main(String[] args) {
 
-    System.out.println("Conflict change");
+    System.out.println("Conflict02 change");
     }
 }
