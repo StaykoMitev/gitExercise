28af541d1f8ad6e2b6c3941dae4d3b3fef70f160
dimabetova <dimabetova@abv.bg>
2022-01-19 15:02:07 +0200

    Commit ClassDDB

--- src/com/test/ClassDDB.java
+++ src/com/test/ClassDDB.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class ClassDDB {
     public static void main(String[] args) {
-        System.out.println("Initial change");
+        System.out.println("Second change");
     }
 }
