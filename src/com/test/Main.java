40dadfda80024f27d20d2c62b578c6f60adad912
mgegova <gegovamargarita@gmail.com>
2022-01-19 18:47:28 +0200

    Commit for Conflict change test

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -3,6 +3,6 @@ package com.test;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Conflict change");
+        System.out.println("Conflict change 4");
     }
 }
